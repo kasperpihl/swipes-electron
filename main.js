cd07a18455f28fe1e8e24f01f21ac3e4ce5a740a
thinklinux <tihomir.valkanov@gmail.com>
2016-05-22 08:54:50 +0300

    making oauth work

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const electron = require('electron');
 const {
   app,
-  BrowserWindow
+  BrowserWindow,
+  ipcMain
 } = electron;
 //const Menu = electron.Menu;
 //const Tray = electron.tray;
@@ -23,19 +24,81 @@ const createWindow = () => {
   win = new BrowserWindow({
     width: 900,
     height: 700,
-    webPreferences: {
-      nodeIntegration: false
-    }
+    title: 'Swipes'
   });
 
-  win.loadURL('https://dev.swipesapp.com/');
+  win.loadURL('http://localhost:3000/');
   win.webContents.openDevTools();
+
+  win.on('page-title-updated', (e) => {
+    e.preventDefault();
+  });
+
   win.on('closed', () => {
     win = null;
   });
 }
 
 app.on('ready', createWindow);
+
+// Handle oauth
+ipcMain.on('oauth-init', (event, arg) => {
+  const handleCallback = (url) => {
+    const raw_code = /code=([^&]*)/.exec(url) || null;
+    const error = /\?error=(.+)$/.exec(url);
+    let code = (raw_code && raw_code.length > 1) ? raw_code[1] : null;
+    let winTitle = null;
+
+    // If there is a code, proceed to get token from github
+    if (code) {
+      winTitle = oauthWin.getTitle();
+      code = decodeURIComponent(code);
+
+      if (winTitle.length === 0) {
+        return;
+      }
+
+      win.webContents.send('oauth-success', {
+        serviceName: winTitle,
+        queryString: {
+          code: code
+        }
+      })
+    } else if (error) {
+      alert('Oops! Something went wrong :/ Please try again.');
+    }
+
+    if (code || error) {
+      // Close the browser if code found or error
+      oauthWin.destroy();
+    }
+  }
+  let oauthWin = new BrowserWindow({
+    width: 800,
+    height: 600,
+    title: arg.serviceName,
+    show: false
+  });
+
+  oauthWin.on('page-title-updated', (e) => {
+    e.preventDefault();
+  });
+
+  oauthWin.webContents.on('will-navigate', function (event, url) {
+    handleCallback(url);
+  });
+
+  oauthWin.webContents.on('did-get-redirect-request', function (event, oldUrl, newUrl) {
+    handleCallback(newUrl);
+  });
+
+  oauthWin.loadURL(arg.url);
+  oauthWin.show();
+
+  oauthWin.on('closed', () => {
+    oauthWin = null;
+  }, false);
+});
 
 //app.on('ready', () => {
   // Tray icon

1006943c46b6e3839090ad958c486a7c3f4613eb
Kasper Pihl Tornøe <kasper@pihl.it>
2016-07-04 17:02:32 -0700

    Fixed minor bug from the refactor

--- preload/tile-preload.js
+++ preload/tile-preload.js
@@ -3,5 +3,5 @@ window.workspaceSendFunction = function(data){
 	ipcRenderer.sendToHost('message', data);
 }
 ipcRenderer.on('message', (event, data) => {
-	swipes._com.receivedCommand(data);
+	swipes.com.receivedCommand(data);
 });
